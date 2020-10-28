78d0fab72ef2c53f695bcb7be1f8a488bc9858d6
joeluca24 <65137591+joeluca24@users.noreply.github.com>
2020-10-28 16:01:00 -0400

    updated package.json

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Home from "./components/Home/Home";
 
 function App() {
   return (
+   <Home/>
     
 
 
---
+++ src/components/Home/Home.js
@@ -0,0 +1,13 @@
+import React,{Fragment}from"react";
+import "./Home.css";
+
+function Home(){
+ return (
+  < Fragment>
+ 
+    <h1>hello world</h1>
+
+
+  </Fragment>
+ )}
+ export default Home;
